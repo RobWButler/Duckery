b3d7293bf980419e82d8376415efc10bc3cb34a3
Derrick <dderrickmatheww@gmail.com>
2019-06-04 19:35:00 -0400

    update on linter

--- models/oauth.js
+++ models/oauth.js
@@ -1,5 +1,5 @@
 module.exports = function(sequelize, DataTypes) {
-  var User = sequelize.define("User", {
+  var User = sequelize.define('User', {
     username: { type: DataTypes.STRING, unique: true },
     password: { type: DataTypes.STRING, allowNull: false },
     email: { type: DataTypes.STRING, unique: true, allowNull: false },
