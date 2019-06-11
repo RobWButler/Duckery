b938a89bafd64f97ca889f6d1f07fbd0c6dc4131
Ethan <ebonsignori@gmail.com>
2019-06-10 20:55:27 -0400

    create duck route

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -13,7 +13,7 @@ module.exports = app => {
   });
 
   // Create duck page
-  app.get('/createduck', function(req, res) {
+  app.get('/duck/create', function(req, res) {
     res.render('createduck', { style: 'styles' });
   });
 
